bbcafc9fe72b4061237a1fca5edee6951df67554
Elliott Octave <octave.elliott2@gmail.com>
2025-04-07 18:18:42 +0200

    Added loader for pages before they are fully loaded

--- Home/static/components/footer.js
+++ Home/static/components/footer.js
@@ -2,4 +2,5 @@ fetch('/Home/static/components/footer.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('footer-container').innerHTML = data;
+    document.dispatchEvent(new Event("footerLoaded"));
   });
--- Home/static/components/navbar.js
+++ Home/static/components/navbar.js
@@ -2,4 +2,5 @@ fetch('/Home/static/components/navbar.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('navbar-container').innerHTML = data;
+    document.dispatchEvent(new Event("navbarLoaded"));
   });
