27cd418ddab331343baafac02fd3d58a4980bbe9
Elliott Octave <octave.elliott2@gmail.com>
2025-05-08 20:12:07 +0200

    added loader to game_details

--- Home/static/scripts/game_details.js
+++ Home/static/scripts/game_details.js
@@ -354,6 +354,10 @@ function setupTimeline() {
 
   renderLineup(0);
   document.getElementById("event-minute").textContent = "0'";
+  document.getElementById('loading-overlay').style.opacity = "0";
+      setTimeout(() => {
+        document.getElementById('loading-overlay').style.display = 'none';
+      }, 500); // makes a smooth transition
   
 }
 
